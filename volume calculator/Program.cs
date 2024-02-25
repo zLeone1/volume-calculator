66942352592b659ab047428963fa7262c9f4329f
Roberto <Roberto@DESKTOP-1818MJE>
2024-02-25 17:31:06 -0600

    Agregar archivos de proyecto.

---
+++ volume calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using volume_calculator;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VolumeCalculator calculator = new VolumeCalculator();
+        calculator.PrintInfo();
+
+        Console.WriteLine("what do you want o calculazte \n 1. Cube \n 2. Cylinder");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                int cubeVolumeInt = (int)calculator.CalculateVolume(5);
+                
+                calculator.PrintVolume(cubeVolumeInt);
+
+                double cubeVolumeDouble = calculator.CalculateVolume(5.5);
+                calculator.PrintVolume(cubeVolumeDouble);
+                break;
+
+
+
+            case 2:
+              
+
+                int cylinderVolumeInt = (int)calculator.CalculateVolume(4, 6);
+                
+                calculator.PrintVolume(cylinderVolumeInt);
+
+                double cylinderVolumeDouble = calculator.CalculateVolume(4.5, 6.5);
+                calculator.PrintVolume(cylinderVolumeDouble);
+
+                break;
+        }
+
+
+       
+
+        
+    }
+}
+
---
+++ volume calculator/Volumecalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace volume_calculator
+{
+    public class VolumeCalculator
+    {
+        public double CalculateVolume(int sideLength)
+        {
+            return Math.Pow(sideLength, 3);
+        }
+
+        public double CalculateVolume(double sideLength)
+        {
+            return Math.Pow(sideLength, 3);
+        }
+
+        public double CalculateVolume(int radius, int height)
+        {
+            return Math.PI * Math.Pow(radius, 2) * height;
+        }
+
+        public double CalculateVolume(double radius, double height)
+        {
+            return Math.PI * Math.Pow(radius, 2) * height;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("This Program calculates the Volume of Cubes and Cylinders");
+            Console.WriteLine("Please, enter the information required to calculate the volumes");
+        }
+        public void PrintVolume(double volume)
+        {
+            Console.WriteLine($"The calculated volume is (double): {volume}");
+        }
+        public void PrintVolume(int volume)
+        {
+            Console.WriteLine($"The calculated volume is (int):{volume}");
+        }
+
+    }
+}
